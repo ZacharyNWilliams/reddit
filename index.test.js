72fb9bf92a16b21e6aeb76daf87ef2a5b8346d6c
ZacharyNWilliams <zacharynwilliams9@gmail.com>
2022-12-04 17:18:48 -0800

    reddit lab

---
+++ index.test.js
@@ -0,0 +1,9 @@
+// let apiCall = require('./script');
+
+
+// test("Displays multiple aww results, each including at least a title", () => {
+
+//     return apiCall().then(list => {
+//         expect(list).toContain('div');
+//       });
+//     });
---
+++ script.js
@@ -0,0 +1,28 @@
+function apiCall(){
+    let url = "https://www.reddit.com/r/aww/.json";
+    fetch(url).then(response => response.json()).then(redditData=> {
+        console.log(redditData);
+
+        for(let i=0;i<10; i++){
+
+        const post = redditData.data.children[i].data
+        const title = document.createElement('h1');
+        const img = document.createElement('img');
+        const url = document.createElement('a');
+        
+            
+        title.innerText = post.title;
+        img.src = post.thumbnail;
+        url.innerText = post.url
+        url.href = post.url
+
+        document.body.appendChild(title)
+        document.body.appendChild(img)
+        document.body.appendChild(url)
+        
+        }
+    })
+} 
+
+apiCall();
+module.exports = apiCall;
